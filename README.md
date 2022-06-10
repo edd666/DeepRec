7bdcbcb3838bbaefceb0403a905a9fec8e0a6e79
liaozhi <liaozhi_edo@163.com>
2022-06-10 17:13:39 +0800

    init

--- README.md
+++ README.md
@@ -1,16 +1,2 @@
-# deeprec
-
-根据推荐系统的不同阶段以及优化目标，可分为：
-
-- 召回（Recall）
-- 排序（Ranking）
-  - 点击率（CTR）
-  - 转化率（CVR）
-  - 多目标（Multi task learning）
-
-
-
-**注意：**
-
-- 对于序列特征，仅支持mask_zero格式--不足长度的补0，不支持直接指定序列长度。
-- 
+# DeepRec-Torch
+基于Pytorch实现推荐相关的深度学习算法。
---
+++ utils.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*- 
+# @Author  : liaozhi
+# @Date    : 2022/6/10
+# @Contact : zhiliao@kugou.net
+
+
+"""
+    Utils
+"""
+
+# packages
+import time
+import datetime
+import numpy as np
+
+
+def get_time_dif(start_time):
+    """
+    获取时间间隔
+    :param start_time: 起始时间
+    :return:
+    """
+    end_time = time.time()
+    time_dif = end_time - start_time
+
+    return datetime.timedelta(seconds=int(round(time_dif)))
+
+
+def build_data_dict(df, varlen_sparse_feature_columns=None):
+    """
+    DataFrame转换成tf模型输入
+
+    :param df:
+    :param varlen_sparse_feature_columns:
+    :return:
+    """
+    # 1,构建模型输入
+    data_dict = dict()
+    for name, value in df.items():
+        value = value.values
+        if varlen_sparse_feature_columns and name in varlen_sparse_feature_columns:
+            data_dict[name] = np.vstack(value)
+        else:
+            data_dict[name] = value
+
+    return data_dict
+
