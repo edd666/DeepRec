22848d9c1c1698d979baab7542be0a527b4c5202
liaozhi <liaozhi_edo@163.com>
2022-06-10 17:21:07 +0800

    init

---
+++ models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- 
+# @Author  : liaozhi
+# @Date    : 2022/6/10
+# @Contact : zhiliao@kugou.net
+
+
+"""
+    TensorFlow模型
+"""
+
+# packages
+
+
---
+++ tf_nn.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*- 
+# @Author  : liaozhi
+# @Date    : 2022/6/10
+# @Contact : zhiliao@kugou.net
+
+
+"""
+    TensorFlow常用代码
+"""
+
+# packages
