3451866322956303c01ab8a559791109ff742b48
liaozhi <liaozhi_edo@163.com>
2022-06-15 12:10:37 +0800

    “v0.1”

--- models.py
+++ models.py
@@ -9,3 +9,50 @@
 """
 
 # packages
+from tf_nn import *
+
+
+def DNN(feature_columns, ):
+
+    # 1,构建输入字典
+    input_dict = build_input_dict(feature_columns)
+
+    # 2,构建embedding dict
+    embedding_dict = build_embedding_dict(feature_columns)
+
+    # 3,构建模型输入
+    # dense
+    dense_value_list = get_dense_value(input_dict, feature_columns)
+
+    # sparse
+    sparse_feature_columns = list(filter(lambda x: isinstance(x, SparseFeat), feature_columns))
+    sparse_embedding_list = embedding_lookup(input_dict, embedding_dict, sparse_feature_columns, to_list=True)
+
+    # seq = varlen sparse
+    # pooling
+    seq_feature_columns = list(filter(lambda x: isinstance(x, VarLenSparseFeat), feature_columns))
+    seq_pooling_embedding_list = get_varlen_pooling_list(input_dict, embedding_dict, seq_feature_columns)
+
+    # concat
+    dnn_embedding_input = concat_func(sparse_embedding_list + seq_pooling_embedding_list + [], mask=False)
+    dnn_embedding_input = layers.Flatten()(dnn_embedding_input)
+    dnn_input = concat_func(dense_value_list + [dnn_embedding_input], mask=False)
+
+    # 4,DNN模型
+    # linear + bn + activation + dropout
+    dnn_output = tf.keras.Sequential(
+        [
+            tf.keras.layers.Dense(3),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.Dropout(0.5)
+        ]
+    )(dnn_input)
+
+    # 5,模型
+    model = tf.keras.Model(
+        inputs=list(input_dict.values()),
+        outputs=dnn_output
+    )
+
+    return model
--- tf_nn.py
+++ tf_nn.py
@@ -209,20 +209,87 @@ def embedding_lookup(input_dict, embedding_dict, query_feature_columns, to_list=
 
 
 class SequencePoolingLayer(layers.Layer):
+    """
+    The SequencePoolingLayer is used to apply pooling operation(sum,mean,max) on variable-length
+    sequence feature/multi-value feature.
+    Input shape
+        - A list of two tensor [seq_value, seq_len]
+        - seq_value is a 3D tensor with shape: (batch_size, T, embedding_size)
+        - seq_len is a 2D tensor with shape : (batch_size, 1),indicate valid length of each sequence.
+    Output shape
+        - 3D tensor with shape: (batch_size, 1, embedding_size).
+    """
 
-    def __init__(self, mode, maxlen, **kwargs):
+    def __init__(self, mode, mask_zero=True, **kwargs):
+        """
+        :param mode: str Pooling方法
+        :param mask_zero: bool 是否支持mask zero
+        :param kwargs:
+        :return:
+        """
         super(SequencePoolingLayer, self).__init__(**kwargs)
+        if mode not in ('sum', 'mean', 'max'):
+            raise ValueError("mode must be sum, mean or max")
         self.mode = mode
-        self.maxlen = maxlen
-        pass
+        self.mask_zero = mask_zero
+        self.seq_maxlen = None
+        self.eps = tf.constant(1e-8, tf.float32)
 
     def build(self, input_shape):
-        pass
+        if not self.mask_zero:
+            self.seq_maxlen = int(input_shape[0][1])
+        super(SequencePoolingLayer, self).build(input_shape)
 
-    def call(self, inputs, **kwargs):
-        pass
+    def call(self, inputs, mask=None, **kwargs):
+        if self.mask_zero:
+            if mask is None:
+                raise ValueError("When mask_zero=True,input must support masking.")
+            seq_value = inputs
+            mask = tf.cast(mask, tf.float32)  # tf.to_float(mask)
+            seq_len = tf.math.reduce_sum(mask, axis=-1, keepdims=True)  # (batch_size, 1)
+            mask = tf.expand_dims(mask, axis=2)
+        else:
+            seq_value, seq_len = inputs
+            mask = tf.sequence_mask(seq_len, self.seq_maxlen, dtype=tf.float32)
+            mask = tf.transpose(mask, (0, 2, 1))
 
-    pass
+        embedding_size = seq_value.shape[-1]
+        mask = tf.tile(mask, [1, 1, embedding_size])
+
+        # max
+        if self.mode == 'max':
+            seq_value = seq_value - (1 - mask) * 1e9
+            return tf.math.reduce_max(seq_value, 1, keepdims=True)
+
+        # sum
+        seq_value = tf.math.reduce_sum(seq_value * mask, 1, keepdims=False)
+
+        # mean
+        if self.mode == 'mean':
+            seq_value = tf.math.divide(seq_value, tf.cast(seq_len, dtype=tf.float32) + self.eps)
+
+        seq_value = tf.expand_dims(seq_value, axis=1)
+
+        return seq_value
+
+    def compute_output_shape(self, input_shape):
+        if self.mask_zero:
+            embedding_size = input_shape[-1]
+        else:
+            embedding_size = input_shape[0][-1]
+
+        shape = (None, 1, embedding_size)
+        return shape
+
+    def compute_mask(self, inputs, mask=None):
+        return None
+
+    def get_config(self):
+
+        config = super(SequencePoolingLayer, self).get_config()
+        config.update({'mode': self.mode, 'mask_zero': self.mask_zero})
+
+        return config
 
 
 def get_varlen_pooling_list(input_dict, embedding_dict, varlen_sparse_feature_columns):
@@ -234,11 +301,51 @@ def get_varlen_pooling_list(input_dict, embedding_dict, varlen_sparse_feature_co
     :return:
     """
     # 1,对VarLenSparseFeat的embedding进行Pooling操作
+    pooling_value_list = []
     for fc in varlen_sparse_feature_columns:
         feature_name = fc.name
         embedding_name = fc.embedding_name
         if fc.weight_name is not None:
             raise ValueError('pooling with weight has not yet been implemented.')
         else:
-            pass
-    pass
+            seq_value = embedding_dict[embedding_name](input_dict[feature_name])
+            pooling_value = SequencePoolingLayer(mode=fc.combiner, mask_zero=True)(seq_value)
+
+        pooling_value_list.append(pooling_value)
+
+    return pooling_value_list
+
+
+class NoMask(layers.Layer):
+    """
+    去掉mask
+    """
+    def __init__(self, **kwargs):
+        super(NoMask, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # Be sure to call this somewhere!
+        super(NoMask, self).build(input_shape)
+
+    def call(self, x, mask=None, **kwargs):
+        return x
+
+    def compute_mask(self, inputs, mask=None):
+        return None
+
+
+def concat_func(inputs, axis=-1, mask=False):
+    """
+    将输入(tensor)合并
+    :param inputs: list 输入tensor
+    :param axis: int axis for concat
+    :param mask: bool 是否支持mask
+    :return:
+    """
+    if not mask:
+        inputs = list(map(NoMask(), inputs))
+
+    if len(inputs) == 1:
+        return inputs[0]
+    else:
+        return tf.keras.layers.Concatenate(axis=axis)(inputs)
